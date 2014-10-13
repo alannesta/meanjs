a44eebb56e1642c635e83381c5f1795d186e3c6b
Alan Cao <alannesta@sina.com>
2014-10-13 17:43:16 -0400

    front-end: editable table (html5 contenteditable attribute)

--- public/scripts/controllers/mainCtrl.js
+++ public/scripts/controllers/mainCtrl.js
@@ -8,4 +8,26 @@ angular.module('customerManagement')
   	}, function(err){
   		console.log(err);
   	});
+
+  	//editable table;
+  	$('#datatable').on('focus', '[contenteditable]', function() {
+  		var $this = $(this);
+  		$this.data('before', $this.html());
+  		$this.addClass("table-editting");
+  		return $this;
+  	}).on('blur', '[contenteditable]', function() {
+  		var $this = $(this);
+  		if ($this.data('before') !== $this.html()) {
+  			$this.data('before', $this.html());
+  			$this.trigger('change');
+  			// console.log("change");
+  		}
+  		$this.removeClass("table-editting");
+  		return $this;
+  	});
+
+  	// $("#datatable").on('change', function(){
+  	// 	alert("change");
+  	// })
+
 }]);
