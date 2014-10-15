660b253f356a5a8814ee64317df8d6640eb2d471
alan cao <acao@moneycloud.me>
2014-10-15 16:49:46 -0400

    use $http rather than $resource mg-bind to the rescue

--- public/scripts/controllers/mainCtrl.js
+++ public/scripts/controllers/mainCtrl.js
@@ -1,52 +1,48 @@
 'use strict';
 
 angular.module('customerManagement')
-  .controller('MainCtrl', ['$scope', '$resource', 'Customer', 'CustomerCollection', function ($scope, $resource, Customer, CustomerCollection) {
+  .controller('MainCtrl', ['$scope', '$resource', '$http', 'Customer', 'CustomerCollection', function ($scope, $resource, $http, Customer, CustomerCollection) {
 
     $scope.addLock = false;
     var currentIndex = -1;   // refer to the current customer under editting
     var previousIndex = -1;
-    var tempStorage = null;
 
-  	CustomerCollection.query(function(data){
-  		$scope.customers = data.reverse();
-  	}, function(err){
-  		console.log(err);
-  	});
+  	// CustomerCollection.query(function(data){
+  	// 	$scope.customers = data.reverse();
+  	// }, function(err){
+  	// 	console.log(err);
+  	// });
+    $http.get('/customers').
+      success(function(data, status, headers, config) {
+          // console.log(status);
+          $scope.customers = data.reverse();
+          // $scope.$apply();
+      }).
+      error(function(data, status, headers, config) {
+          console.log(status);
+      });
+
+
 
     $scope.addCustomer = function(){
-
+      if (currentIndex>-1){
+        $scope.customers[currentIndex].editting = false;
+      }
+      
+      
       if($scope.addLock) return;
       getLastCid(function(result){
         var newid = result + 1;
         var customerObj = {
           cid: newid,
-          name: "",
-          phone: "",
-          weichat: "",
-          note: "",
+          name: null,
+          phone: null,
+          weichat: null,
+          note: null,
           editting: true
         }
         $scope.customers.unshift(customerObj);
-
-        // var trDom = "<tr><td>"+newid+"</td>"+
-        //           "<td contenteditable='true' ng-bind = 'customer.name'></td>"+
-        //           "<td contenteditable='true' ng-bind = 'customer.phone'></td>"+
-        //           "<td contenteditable='true' ng-bind = 'customer.weichat'></td>"+
-        //           "<td contenteditable='true' ng-bind = 'customer.name'></td>"+
-        //           "<td contenteditable='true' ng-bind = 'customer.note'></td></tr>";
-        // $("#datatable tbody").prepend($(trDom));
-        // $("tbody tr td")[1].focus();
-
-        // var button = $("<td><button class='btn btn-success' id='savebtn'>Save</button></td>");
-        // button.on('click', function(){
-        //   console.log("save");
-        // })
-        // var parent = $($("tbody tr")[0])
-        // parent.find("button").css("display","none");
-        // parent.append(button);
-
-        // console.log($("tbody tr")[0]);
+        console.log($scope.customers);
 
       })
       $scope.addLock = true;
@@ -54,10 +50,25 @@ angular.module('customerManagement')
 
     //save button handler
     $scope.saveCustomer = function(customer, index){
-      console.log("called");
-      console.log($scope.customers[index]);
+
+      if (currentIndex>-1){
+        
+        $scope.customers[currentIndex].editting = false;
+      }
+      
+      var dom = $($("tbody").find('tr')[index]);
+      // console.log("before");
+      // console.log($scope.customers[index]);
+      $scope.customers[index].cid= dom.find('td')[0].innerHTML
+      $scope.customers[index].name= dom.find('td')[1].innerHTML
+      $scope.customers[index].phone= dom.find('td')[2].innerHTML
+      $scope.customers[index].weichat= dom.find('td')[3].innerHTML
+      $scope.customers[index].note= dom.find('td')[5].innerHTML
       upsertCustomer({cid: $scope.customers[index].cid}, $scope.customers[index])
-      $scope.customers[currentIndex].editting = false;
+      $scope.customers[index].editting = false;
+      // console.log("after");
+      // console.log($scope.customers[index]);
+      $scope.addLock = false;
     }
 
     //cancel button handler
@@ -82,8 +93,7 @@ angular.module('customerManagement')
   	//editable table;
   	$('#datatable').on('focus', '[contenteditable]', function(event) {
   		var $this = $(this);
-      var $that = $(this);  
-      console.log(tempStorage);
+      var $that = $(this);
       //get the index of the current editted tr in the dom tree(to match the customer in the customers array)
       $("tbody").find("tr").each(function(index, item){
         if ($(this).find("td")[0].innerHTML == $that.parent().find("td")[0].innerHTML){
@@ -91,54 +101,45 @@ angular.module('customerManagement')
         }
       });
       if (previousIndex != currentIndex){
-        console.log("previous: " + previousIndex + " current: "+ currentIndex);
+        if (previousIndex == -1){
+          $scope.customers[0].editting = false;
+        }else{
+          $scope.customers[previousIndex].editting = false;
+        }
+        
         if (previousIndex > -1){
           // debugger;
-          $scope.customers[previousIndex] = angular.copy(tempStorage);
-          $scope.customers[previousIndex].editting = false;
+          // console.log("previous: " + previousIndex + " current: "+ currentIndex);
+          // console.log(tempStorage);
+          // $scope.customers[previousIndex] = angular.copy(tempStorage);
+          // console.log($scope.customers[previousIndex]);
+          // $scope.customers[previousIndex].editting = false;
           // $scope.$apply();
         }
       }
 
+      // $scope.customers[currentIndex].cid= $this.parent().find('td')[0].innerHTML
+      // $scope.customers[currentIndex].name= $this.parent().find('td')[1].innerHTML
+      // $scope.customers[currentIndex].phone= $this.parent().find('td')[2].innerHTML
+      // $scope.customers[currentIndex].weichat= $this.parent().find('td')[3].innerHTML
+      // $scope.customers[currentIndex].note= $this.parent().find('td')[5].innerHTML
+
+      previousIndex = currentIndex;
       // console.log("previous: " + previousIndex + " current: "+ currentIndex);
       $this.addClass("table-editting");
-      tempStorage = angular.copy($scope.customers[currentIndex]);
+      // tempStorage = angular.copy($scope.customers[currentIndex]);
       $scope.customers[currentIndex].editting = true;
       $scope.$apply();
 
   		return $this;
   	})
 
-    $('#datatable').on('blur', '[contenteditable]', function() {
-
-      var $this = $(this);
-      var $that = $(this); 
-
-      $scope.customers[currentIndex].cid= $this.parent().find('td')[0].innerHTML
-      $scope.customers[currentIndex].name= $this.parent().find('td')[1].innerHTML
-      $scope.customers[currentIndex].phone= $this.parent().find('td')[2].innerHTML
-      $scope.customers[currentIndex].weichat= $this.parent().find('td')[3].innerHTML
-      $scope.customers[currentIndex].note= $this.parent().find('td')[5].innerHTML
-      
-      $("tbody").find("tr").each(function(index, item){
-        if ($(this).find("td")[0].innerHTML == $that.parent().find("td")[0].innerHTML){
-          previousIndex = index;
-        }
-      });
-
-      // console.log($scope.currentFocus);
-      // $scope.currentFocus.editting = false;
-      $scope.$apply();
-  		$this.removeClass("table-editting");
-  		return $this;
-  	});
-
     /*
       update or insert the customer into the database;
     */
   	function upsertCustomer(keyObj, customerObj){
       var customer = Customer.get(keyObj, function(result){
-          console.log(result);
+          // console.log(result);
           // data[0].note = "changed successfully";
 
           if (result.length == 1){
@@ -149,13 +150,26 @@ angular.module('customerManagement')
             result[0].$save();
           }else if (result.length == 0){    //no previous record under the phone or weichat, create a new resource
             console.log("insert")
-            var newCustomer = new Customer();
-            newCustomer.cid = customerObj.cid;
-            newCustomer.name = customerObj.name;
-            newCustomer.phone = customerObj.phone;
-            newCustomer.weichat = customerObj.weichat;
-            newCustomer.note = customerObj.note;
-            newCustomer.$save();
+            // var newCustomer = new Customer();
+            // newCustomer.cid = customerObj.cid;
+            // newCustomer.name = customerObj.name;
+            // newCustomer.phone = customerObj.phone;
+            // newCustomer.weichat = customerObj.weichat;
+            // newCustomer.note = customerObj.note;
+            // newCustomer.$save();
+            $http.post('/customers/'+ customerObj.cid, {
+                cid: customerObj.cid,
+                name: customerObj.name,
+                phone: customerObj.phone,
+                weichat: customerObj.weichat,
+                note: customerObj.note,
+            }).
+            success(function(data, status, headers, config){
+
+            }).
+            error(function(data, status, headers, config){
+                console.log(status);
+            });
           }
           
         })
