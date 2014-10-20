9b8d5b18afd5b6d5a0ebd8d2600d5870c361c512
alan cao <acao@moneycloud.me>
2014-10-20 12:29:52 -0400

    finish the entire edit logic, seems to work alright, need to add test cases for different scenarios

--- public/scripts/controllers/mainCtrl.js
+++ public/scripts/controllers/mainCtrl.js
@@ -4,8 +4,8 @@ angular.module('customerManagement')
   .controller('MainCtrl', ['$scope', '$resource', '$http', 'Customer', 'CustomerCollection', 'Calendar', function ($scope, $resource, $http, Customer, CustomerCollection, Calendar) {
 
     $scope.addLock = false;
-    var currentIndex = -1;   // refer to the current customer under editting
-    var previousIndex = -1;
+    $scope.dblclickLock = false;
+    $scope.addFlag = false;
     var tempStorage = {};
 
   	// CustomerCollection.query(function(data){
@@ -26,10 +26,6 @@ angular.module('customerManagement')
 
 
     $scope.addCustomer = function(){
-      if (currentIndex>-1){
-        $scope.customers[currentIndex].editting = false;
-      }
-      
       
       if($scope.addLock) return;
       getLastCid(function(result){
@@ -40,53 +36,18 @@ angular.module('customerManagement')
           phone: null,
           weichat: null,
           appointment: null,
-          note: null,
-          editting: true
+          note: null
         }
         $scope.customers.unshift(customerObj);
         console.log($scope.customers);
-
+        // $scope.addLock = true;
+        // $scope.dblclickLock = true;
+        // customerObj.editting = true;
+        $scope.addFlag = true;
+        startEditting(customerObj);
       })
-      $scope.addLock = true;
     }
 
-    //save button handler
-    // $scope.saveCustomer = function(customer, index){
-
-    //   if (currentIndex>-1){
-    //     $scope.customers[currentIndex].editting = false;
-    //   }
-      
-    //   var dom = $($("tbody").find('tr')[index]);
-    //   var appointment = ""
-    //   if(dom.find('div').length>0){
-    //     appointment = dom.find('div').html();
-    //     console.log(" from div"+ appointment);
-    //   }else{
-
-    //     appointment = dom.find('input').val();
-    //     console.log(" from input"+ appointment);
-    //   }
-        
-    //   appointment = moment(appointment).toISOString();  //转化为ISO格式
-    //   if (appointment == 'Invalid date'){
-    //     appointment = ""
-    //   }
-   
-    //   // console.log("before");
-    //   // console.log($scope.customers[index]);
-    //   $scope.customers[index].cid= dom.find('td')[0].innerHTML
-    //   $scope.customers[index].name= dom.find('td')[1].innerHTML
-    //   $scope.customers[index].phone= dom.find('td')[2].innerHTML
-    //   $scope.customers[index].weichat= dom.find('td')[3].innerHTML
-    //   $scope.customers[index].appointment = appointment
-    //   $scope.customers[index].note= dom.find('td')[5].innerHTML
-    //   upsertCustomer({cid: $scope.customers[index].cid}, $scope.customers[index])
-    //   $scope.customers[index].editting = false;
-    //   // console.log("after");
-    //   // console.log($scope.customers[index]);
-    //   $scope.addLock = false;
-    // }
 
     $scope.saveCustomer = function(customer, index){
       if (customer.appointment){
@@ -94,19 +55,47 @@ angular.module('customerManagement')
       }
       
       upsertCustomer({cid: customer.cid}, customer, function successHandler(){
-        $scope.addLock = false;
-        customer.editting = false;
+        // $scope.addLock = false;
+        // customer.editting = false;
+        // $scope.dblclickLock = false;
+        endEditting(customer);
+        $scope.addFlag = false;
       })
     }
 
-
     //cancel button handler
-    $scope.cancelEdit = function(){
+    $scope.cancelEdit = function(customer){
+      console.log(customer)
+      // customer = angular.copy(tempStorage);  // this will not work because change the reference itself. it points to a different thing now
+      for (var prop in tempStorage){
+        customer[prop] = tempStorage[prop];
+      }
 
+      
+
+      //if in adding phase, shift the first obj when cancel
+      if ($scope.addFlag){
+        $scope.customers.shift($scope.customers[0]);
+      }
+      endEditting(customer);
+      $scope.addFlag = false;
+      // customer.editting = false;
+      // $scope.addLock = false;
+      // $scope.dblclickLock = false;
+      console.log(customer);
     }
 
     $scope.editCustomer = function(customer){
-      customer.editting = true;
+      // tempStorage = angular.copy(customer);
+      // tempStorage = angular.extend({}, customer)
+      if ($scope.dblclickLock){
+        return;
+      }
+      tempStorage = JSON.parse(JSON.stringify(customer));   //make a copy of the current one
+      // $scope.addLock = true;
+      // customer.editting = true;
+      // $scope.dblclickLock = true;
+      startEditting(customer);
     }
 
     //delete button handler
@@ -151,44 +140,7 @@ angular.module('customerManagement')
       }
 
     }
-  	//editable table;
-  	// $('#datatable').on('focus', '[contenteditable]', function(event) {
-  	// 	var $this = $(this);
-   //    var $that = $(this);
-   //    //get the index of the current editted tr in the dom tree(to match the customer in the customers array)
-   //    $("tbody").find("tr").each(function(index, item){
-   //      if ($(this).find("td")[0].innerHTML == $that.parent().find("td")[0].innerHTML){
-   //        currentIndex = index;
-   //      }
-   //    });
-   //    if (previousIndex != currentIndex){
-   //      if (previousIndex == -1){
-   //        $scope.customers[0].editting = false;
-   //      }else{
-   //        $scope.customers[previousIndex].editting = false;
-   //      }
-        
-   //      if (previousIndex > -1){
-   //        // debugger;
-   //        // console.log("previous: " + previousIndex + " current: "+ currentIndex);
-   //        // console.log(tempStorage);
-   //        // $scope.customers[previousIndex] = angular.copy(tempStorage);
-   //        // console.log($scope.customers[previousIndex]);
-   //        // $scope.customers[previousIndex].editting = false;
-   //        // $scope.$apply();
-   //      }
-   //    }
 
-   //    previousIndex = currentIndex;
-   //    // console.log("previous: " + previousIndex + " current: "+ currentIndex);
-   //    // $this.addClass("table-editting");
-   //    // tempStorage = angular.copy($scope.customers[currentIndex]);
-   //    $scope.customers[currentIndex].editting = true;
-   //    $scope.$apply();
-
-  	// 	return $this;
-  	// })
-  
     $scope.focusHandler = function(customer){
       
     }
@@ -197,9 +149,6 @@ angular.module('customerManagement')
       console.log("blur");
       // customer.editting = false;
     }
-
-
-
 
     /*
       update or insert the customer into the database;
@@ -216,7 +165,6 @@ angular.module('customerManagement')
             result[0].appointment = customerObj.appointment;
             result[0].note = customerObj.note;
             result[0].$save();
-            
             callback();
           }else if (result.length == 0){    //no previous record under the phone or weichat, create a new resource
             console.log("insert")
@@ -243,6 +191,18 @@ angular.module('customerManagement')
     }
 
     //utility functions
+    function startEditting(customer){
+      $scope.addLock = true;
+      customer.editting = true;
+      $scope.dblclickLock = true;
+    }
+
+    function endEditting(customer){
+      $scope.addLock = false;
+      customer.editting = false;
+      $scope.dblclickLock = false;
+    }
+
 
     //get the last(largest) cid in db
     function getLastCid(callback){
@@ -255,7 +215,6 @@ angular.module('customerManagement')
           }
         })
         callback(largestCid);
-        // return largestCid;
       })
     }
 
--- public/scripts/filters/dateFilter.js
+++ public/scripts/filters/dateFilter.js
@@ -1,6 +1,11 @@
 app.filter('formatDate', function() {
 	return function(date) {
-		var momentStr = new moment(date);		//utilize moment.js
-		return momentStr.format('YYYY-MM-DD');
+		if (date){
+			var momentStr = new moment(date);		//utilize moment.js
+			return momentStr.format('YYYY-MM-DD');
+		}else{
+			return "No Date Selected"
+		}
+		
 	};
 });
